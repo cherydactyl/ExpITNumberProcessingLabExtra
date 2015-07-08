b8c30447fed9da393f0b2b7fe470af787baf20cd
cherydactyl <cheryl.orosz@gmail.com>
2015-07-07 21:03:41 -0400

    InitialProject
    Takes 3 arguments, which are interpreted as the coefficients a, b, and c
    in the quadratic equation.  Uses the quadratic formula to find
    solutions, if they exist.

---
+++ ExpITNumberProcessingLabExtra/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpITNumberProcessingLabExtra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //A program for solving the Quadratic equation
+            /*
+             * expects arguments a, b, and c, in that order, as integers
+             * 
+             * The quadratic formula
+             *      X = [-b +/- Sqrt(b^2 - 4ac)] / 2a
+             * solves for the root(s) X of equations in the form
+             *      a(X^2) + bX + c = 0
+             *      
+             * Depending on whether the discriminant (b^2 - 4ac) is
+             *      negative, zero, or positive,
+             *      there will be 0, 1, or 2 solutions for X
+             *      
+             * Note the special case when a, b, and c are all 0:
+             *      any X is a solution
+             */
+
+            Console.WriteLine("Extra Credit: solve the quadratic equation");
+            
+            //no error handling; naive processing of input
+            int a = Convert.ToInt32(args[0]);
+            int b = Convert.ToInt32(args[1]);
+            int c = Convert.ToInt32(args[2]);
+
+            //show the equation to be solved, reflecting the arguments as passed in
+            //making it pretty if either or both b and/or c are negative
+            Console.WriteLine("Equation to solve:");
+            Console.Write(a + "X^2 ");
+            if (b < 0)
+            {
+                Console.Write("- " + (-b) + "X ");
+            }
+            else
+            {
+                Console.Write("+ " + b + "X ");
+            }
+            if (c < 0)
+            {
+                Console.Write("- " + (-c));
+            }
+            else
+            {
+                Console.Write("+ " + c);
+            }
+            Console.Write(" = 0");
+
+            Console.WriteLine();    //whitespace
+
+            //check for all 0 coefficients
+            if (a == 0 && b == 0 && c == 0)
+            {
+                Console.Write("For those values, any X is a solution.  Nice Try.");
+            }   //otheriwse check status of discriminant: negative, zero, or positive
+            else
+            {   //a, b, and c are not all equal to 0, so proceed
+                //caluculate discriminant
+                int discriminant = (b * b) - (4 * a * c);
+                //Console.WriteLine("Discriminant = " + discriminant);  //for debugging check of discriminant
+
+                if (discriminant < 0)
+                {
+                    Console.WriteLine("There is no solution.");
+                }
+                else if (discriminant == 0)
+                {
+                    Console.WriteLine("There is exactly one solution: X = " + -b / (2 * a));
+                }
+                else
+                {
+                    Console.WriteLine("There are two solutions:");
+                    Console.WriteLine("X = " + (-b + Math.Sqrt(discriminant)) / (2 * a) + " and");
+                    Console.WriteLine("X = " + (-b - Math.Sqrt(discriminant)) / (2 * a));
+
+                }
+            }
+
+            Console.WriteLine();    //whitespace
+            Console.WriteLine();    //whitespace
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+    
+        }
+    }
+}
